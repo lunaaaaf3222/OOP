54522d4d78c4614ac0a681377ffa79d12044264f
lunaaaaf3222 <lucaforo40mary@gmail.com>
2026-02-18 15:48:38 -0500

    Add files via upload

---
+++ Corte1/Julio/Numero.java
@@ -0,0 +1,24 @@
+public class Numero {
+   //Campo o atributo
+    private int numero;
+
+    //constructores
+
+    public Numero() {
+
+    }
+
+    public Numero(int numero) {
+        this.numero = numero;
+    }
+
+    //METODOS
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+}
