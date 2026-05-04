60ae9a6906ac7931762be985fbaee1cd3613fe7d
lunaaaaf3222 <lucaforo40mary@gmail.com>
2026-05-04 00:30:49 -0500

    Add files via upload

---
+++ Corte2/Java/Bank/CheckingAccount.java
@@ -0,0 +1,27 @@
+public class CheckingAccount{
+    private String name;
+    private int balance;
+    private String id;
+
+    public CheckingAccount(String inputName, int inputBalance, String inputId){
+        name = inputName;
+        balance = inputBalance;
+        id = inputId;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int newBalance){
+        balance = newBalance; 
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+  
+}
